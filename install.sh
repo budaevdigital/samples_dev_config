7fd5b40a52797e70a52be83b6fb66e7da91cfb60
budaevdigital <budaev.digital@gmail.com>
2024-03-31 02:02:58 +0500

    build: обновил скрипт для распаковки проекта в нужную директорию

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-CURRENT_DIR=.
-PROJECT_FILE_ZIP=
+CURRENT_DIR=$PWD
+PROJECT_FILE_ZIP=samples_dev_config-master.zip
 
 wget https://github.com/budaevdigital/samples_dev_config/archive/master.zip -O $CURRENT_DIR
-unzip $PROJECT_FILE_ZIP -d .
+unzip $PROJECT_FILE_ZIP -d $CURRENT_DIR
 rm $PROJECT_FILE_ZIP
 
 echo "Done!"
